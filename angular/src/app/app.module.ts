7bf30dde09ae57392361eb23e0bcdff4a34380c5
Mike Chirico <mchirico@gmail.com>
2020-04-03 14:34:25 -0400

    Angular Navbar (#12)

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './navpages/auth/auth.component';
+import { Page0Component } from './navpages/page0/page0.component';
+import { Page1Component } from './navpages/page1/page1.component';
+import { MainComponent } from './navpages/main/main.component';
+import { SearchComponent } from './navpages/search/search.component';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    Page0Component,
+    Page1Component,
+    MainComponent,
+    SearchComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
